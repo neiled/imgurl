0ccde53752812b63f5b172ac0136c07897397ab9
Neil Edwards <neil@plasticwater.com>
2021-12-24 17:06:50 -0500

    Initial commit

---
+++ src/routes/profile/[id].svelte
@@ -0,0 +1,10 @@
+<script context="module">
+  // your script goes here
+  export async function load({ page, fetch, session, context }) {
+    console.log('loading profile/[id]');
+    console.log(page.params);
+    return {};
+  }
+</script>
+
+<div class="" style="background-image: url('nothing that exists');" />
